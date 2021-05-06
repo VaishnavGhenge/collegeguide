46c2e14c3f0c5853c863831bdc0534e2caa2b547
Vaishnav Ghenge <vaishnavghenge12@gmail.com>
2021-05-06 21:13:25 +0530

    Updated

--- update.py
+++ update.py
@@ -2,5 +2,5 @@ import os
 os.system('git add .')
 os.system('git commit -m "Updated"')
 os.system("git push origin -u main")
-os.system("clear")
+os.system("cls")
 print("Repository Updated Successfully...")
